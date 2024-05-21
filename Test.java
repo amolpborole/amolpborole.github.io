f7cd2a9458cd539c8c433d65d836649180476470
APB <amol.borole@gmail.com>
2024-05-21 14:41:38 -0700

    Create Test.java
    test

---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	public static void main(String[] args) {
+	    test1();
+	}
+	
+	public static void test1() {
+        
+        String name = "abc";
+        
+        System.out.println(String.format("formatted message, name=%s", name));
+	}
+
+}
